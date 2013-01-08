316cad1642b99ab7437a9dfbf89db99cd9ebd57f
Dainius Simelevicius <dainius.simelevicius@gmail.com>
2013-01-08 12:27:20 +0200

    Atliktos parametrų konversijos į dinaminei bibliotekai tinkamus matavimo vienetus

--- calculator_runner.cpp
+++ calculator_runner.cpp
@@ -43,27 +43,40 @@ Q_INVOKABLE void CalculatorRunner::setBiosensorInformation(int explicitScheme, i
     biosensorInformation->explicitScheme = explicitScheme;
     biosensorInformation->substrateInhibition = substrateInhibition;
     biosensorInformation->productInhibition = productInhibition;
+    //[s^-1]
     biosensorInformation->k2 = k2;
-    biosensorInformation->kM = kM;
-    biosensorInformation->kS = kS;
-    biosensorInformation->kP = kP;
+    //[mol/l] -> [mol/cm^3]
+    biosensorInformation->kM = kM * 1e-3;
+    //[mol/l] -> [mol/cm^3]
+    biosensorInformation->kS = kS * 1e-3;
+    //[mol/l] -> [mol/cm^3]
+    biosensorInformation->kP = kP * 1e-3;
+    //[s]
     biosensorInformation->timeStep = timeStep;
     biosensorInformation->N = N;
     biosensorInformation->responseTimeMethod = responseTimeMethod;
+    //[s]
     biosensorInformation->minTime = minTime;
+    //[s]
     biosensorInformation->responseTime = responseTime;
     biosensorInformation->outputFileName = outputFileName.toLocal8Bit().data();
     biosensorInformation->ne = ne;
-    biosensorInformation->s0 = s0;
-    biosensorInformation->p0 = p0;
+    //[mol/l] -> [mol/cm^3]
+    biosensorInformation->s0 = s0 * 1e-3;
+    //[mol/l] -> [mol/cm^3]
+    biosensorInformation->p0 = p0 * 1e-3;
     biosensorInformation->noOfBiosensorLayers = noOfBiosensorLayers;
     biosensorInformation->biosensorLayers = (struct LayerInformation *) malloc(sizeof(struct LayerInformation) * noOfBiosensorLayers);
 }
 
 Q_INVOKABLE  void CalculatorRunner::setLayerInformation(int index, int enzymeLayer, double Ds, double Dp, double d, double e0) {
     biosensorInformation->biosensorLayers[index].enzymeLayer = enzymeLayer;
-    biosensorInformation->biosensorLayers[index].Ds = Ds;
-    biosensorInformation->biosensorLayers[index].Dp = Dp;
-    biosensorInformation->biosensorLayers[index].d = d;
-    biosensorInformation->biosensorLayers[index].e0 = e0;
+    //[um^2/s] -> [cm^2/s]
+    biosensorInformation->biosensorLayers[index].Ds = Ds * 1e-8;
+    //[um^2/s] -> [cm^2/s]
+    biosensorInformation->biosensorLayers[index].Dp = Dp * 1e-8;
+    //[um] -> [cm]
+    biosensorInformation->biosensorLayers[index].d = d * 1e-4;
+    //[mol/l] -> [mol/cm^3]
+    biosensorInformation->biosensorLayers[index].e0 = e0 * 1e-3;
 }
